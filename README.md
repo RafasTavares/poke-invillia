6b98b4d32481913c0167eb7b408b9573ef4db5a5
Rafael Tavares <rafaeltavaresandrade@gmail.com>
2020-05-10 22:00:37 -0300

    Delete README.md

--- README.md
+++
@@ -1,3 +0,0 @@
-# poke-Invillia Project
-
-> API utilizada: [https://pokeapi.co](https://pokeapi.co) :zap:# poke-invillia
--- src/app/model/pokedex.model.ts
+++
@@ -1,8 +0,0 @@
-import { Pokemon } from './pokemon.model';
-
-export class Pokedex {
-    name: string;
-    next: string;
-    previous: string;
-    results: Pokemon[];
-}
